d31c0b75bceaa15d949cb18622173fcb17392ce4
Camilo Ballesteros <c.ballesteros@usacelia.com>
2020-09-15 19:31:03 -0500

    first commit

---
+++ src/data/index.ts
@@ -0,0 +1,9 @@
+import { SeasonsData } from './data-season';
+import { RacesData } from './data-races';
+import { DriversData } from './data-drivers';
+
+export const dataSources = {
+    SeasonsData,
+    RacesData,
+    DriversData
+}
---
+++ src/resolvers/type.ts
@@ -0,0 +1,31 @@
+import { IResolvers } from 'graphql-tools';
+import { getWikipediaMobileUrl } from '../lib/utils';
+
+const type: IResolvers = {
+    Season: {
+        year: parent => parent.season,
+        urlMobile: parent => getWikipediaMobileUrl(parent.url)
+    },
+    Race: {
+        name: parent => parent.raceName,
+        circuit: parent => parent.Circuit,
+        urlMobile: parent => getWikipediaMobileUrl(parent.url),
+        // results: parent => parent.Results
+    },
+    Circuit: {
+        id: parent => parent.circuitId,
+        name: parent => parent.circuitName,
+        location: parent => parent.Location,
+        urlMobile: parent => getWikipediaMobileUrl(parent.url)
+    },
+    Location: {
+        lng: parent => parent.long
+    },
+    Driver: {
+        id: parent => parent.driverId,
+        name: parent => parent.givenName.concat(parent.familyName),
+        urlMobile: parent => getWikipediaMobileUrl(parent.url)
+    }
+};
+
+export default type;
